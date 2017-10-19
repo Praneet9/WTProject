c0b1c1c2ec41ff2fa682d34b7212663445e48779
Praneet9 <praneetbomma@gmail.com>
2017-10-20 00:45:24 +0530

    signin and userfeed completed

---
+++ components/feed.php
@@ -0,0 +1,26 @@
+
+<?php
+//Step1
+ $db = mysqli_connect('localhost','root','','database_name')
+ or die('Error connecting to MySQL server.');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Feed</title>
+</head>
+<body>
+
+    <ul>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+    </ul>
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+   $(".tab").click(function () {
+       var x = $(this).attr('id');
+       if(x == 'signuptab'){
+           $('#signintab').removeClass('select');
+           $('#signuptab').addClass('select');
+           $('#signupbox').fadeIn(600);
+           $('#signinbox').fadeOut(0);
+       }
+       if(x == 'signintab'){
+           $('#signuptab').removeClass('select');
+           $('#signintab').addClass('select');
+           $('#signupbox').fadeOut(0);
+           $('#signinbox').fadeIn(600);
+       }
+   })
+});
